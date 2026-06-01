39c460f110b89e5ffb59b877d6355faf808cb1b3
abe-ntu <r14853003@ntu.edu.tw>
2026-06-01 15:01:07 +0800

    analysis.R
    This script imports a dataset and calculates the total number of dengue cases for the year 2023.

---
+++ analysis.R
@@ -0,0 +1,10 @@
+library (readr)
+library(dplyr)
+
+# Import dataset
+dengue <- read.csv("dengue_assignment.csv", header = TRUE)
+
+# Calculate total dengue cases in 2023
+total_2023 <- sum(dengue$case_number[dengue$year == 2023])
+# display result
+total_2023 
